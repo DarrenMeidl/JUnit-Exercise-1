29a382cfca06e117539810dc06ea739868a150c5
DarrenMeidl <dmeidl15@gmail.com>
2023-10-06 12:10:38 +0100

    Created multiple boolean method, created 8 tests that pass

--- src/JUnit.java
+++ src/JUnit.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class JUnit {
+public class JUnit {
+    //Q1 METHOD
+    public boolean multiple(int num){
+        if (num % 5 == 0 || num % 3 == 0){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    //Q2 METHOD
+    public int
 }
---
+++ src/JUnitTest.java
@@ -0,0 +1,98 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class JUnitTest {
+    //Q1 METHOD TESTS
+    /*
+    @Test
+    void numEqualsOne() {
+        System.out.println("TEST 1: Num = 1");
+        int num = 1;
+        Boolean expected = false;
+        JUnit j = new JUnit();
+        Boolean actual = j.multiple(num);;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void numEqualsTwo() {
+        System.out.println("TEST 2: Num = 2");
+        int num = 2;
+        Boolean expected = false;
+        JUnit j = new JUnit();
+        Boolean actual = j.multiple(num);;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void numEqualsThree() {
+        System.out.println("TEST 3: Num = 3");
+        int num = 3;
+        Boolean expected = true;
+        JUnit j = new JUnit();
+        Boolean actual = j.multiple(num);;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void numEqualsFive() {
+        System.out.println("TEST 4: Num = 5");
+        int num = 5;
+        Boolean expected = true;
+        JUnit j = new JUnit();
+        Boolean actual = j.multiple(num);;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void numEqualsFifteen() {
+        System.out.println("TEST 5: Num = 15");
+        int num = 15;
+        Boolean expected = true;
+        JUnit j = new JUnit();
+        Boolean actual = j.multiple(num);;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void numEqualsMinusSeven() {
+        System.out.println("TEST 6: Num = -7");
+        int num = -7;
+        Boolean expected = false;
+        JUnit j = new JUnit();
+        Boolean actual = j.multiple(num);;
+        assertEquals(expected, actual);
+    }
+    @Test
+    void numEqualsMinusSixtyFive() {
+        System.out.println("TEST 7: Num = -65");
+        int num = -65;
+        Boolean expected = true;
+        JUnit j = new JUnit();
+        Boolean actual = j.multiple(num);;
+        assertEquals(expected, actual);
+    }
+    @Test
+    void numEqualsZero() {
+        System.out.println("TEST 8: Num = 0");
+        int num = 0;
+        Boolean expected = true;
+        JUnit j = new JUnit();
+        Boolean actual = j.multiple(num);;
+        assertEquals(expected, actual);
+    }
+
+     */
+
+
+
+
+
+
+    //Q2 METHOD TESTS
+    @Test
+    void nice(){
+
+    }
+
+}
